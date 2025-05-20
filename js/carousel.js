5dfb3ae60872f7c89fe84eded3b9995eb14db8f5
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-20 08:41:44 +0200

    feat : added the loop 'for' for the carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,9 @@
 function showSlide(index) {
-
     const slides = document.querySelectorAll('#carousel li');
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
+
     slides[index].style.display = 'block';
 }
-let currentIndex = 0;
-
-showSlide(currentIndex);
-
-document.getElementById('next').addEventListener('click', function() {
-    currentIndex++;
-    
-    if (currentIndex >= document.querySelectorAll('#carousel li').length) {
-        currentIndex = 0;
-    }
-    showSlide(currentIndex);
-});
-
-document.getElementById('previous').addEventListener('click', function() {
-    currentIndex--;
-    
-    if (currentIndex < 0) {
-        currentIndex = document.querySelectorAll('#carousel li').length;
-    }
-    showSlide(currentIndex);
-});
